d18c23980c3e4b9983a9bff15436622bc2eeb93e
Stuart Grange <s.k.grange@gmail.com>
2024-03-01 14:44:57 +0100

    :sparkles: enhance import_meteorological_data_by_site with messaging & new arguments & the handling of sites with no processes

--- R/import_meteorological_data_by_site.R
+++ R/import_meteorological_data_by_site.R
@@ -24,15 +24,25 @@
 #' 
 #' @param valid_only Should invalid observations be filtered out? 
 #' 
+#' @param set_invalid_values Should invalid observations be set to \code{NA}? 
+#' 
 #' @param tz Time-zone for the dates to be parsed into. Default is \code{"UTC"}. 
+#' 
+#' @param verbose Should the function give messages? 
+#' 
+#' @param progress Should a progress bar be displayed? 
 #' 
 #' @return Tibble. 
 #' 
 #' @export
 import_meteorological_data_by_site <- function(con, site, summary = 1L, 
                                                start = 1969, end = NA,
-                                               variables = c("air_temp", "rh", "wd", "ws"),
-                                               valid_only = FALSE, tz = "UTC") {
+                                               variables = c("air_temp", "rh", "wd", "ws", "global_radiation"),
+                                               valid_only = FALSE, 
+                                               set_invalid_values = FALSE, 
+                                               tz = "UTC",
+                                               verbose = FALSE,
+                                               progress = FALSE) {
   
   # Get sites table
   df_sites <- smonitor::import_sites(con)
@@ -49,8 +59,11 @@ import_meteorological_data_by_site <- function(con, site, summary = 1L,
       summary = summary,
       variables = variables,
       valid_only = valid_only,
-      tz = tz
-    )
+      set_invalid_values = set_invalid_values,
+      tz = tz,
+      verbose = verbose
+    ),
+    .progress = progress
   ) %>% 
     purrr::list_rbind()
   
@@ -58,13 +71,27 @@ import_meteorological_data_by_site <- function(con, site, summary = 1L,
 
 
 import_sites_met_worker <- function(con, site, df_sites, start, end, summary, 
-                                    variables, valid_only, tz) {
+                                    variables, valid_only, set_invalid_values, 
+                                    tz, verbose) {
+  
+  # Message to user
+  if (verbose) {
+    cli::cli_alert_info("{threadr::cli_date()} `{site}...`")
+  }
   
   # Filter the sites table
   df_site <- filter(df_sites, site == !!site)
   
   # Switch site if needed
   if (!is.na(df_site$site_pair)) {
+    # Message the change
+    if (verbose) {
+      cli::cli_alert_info(
+        "{threadr::cli_date()} Importing `{site}`'s paired `{df_site$site_pair}` site..."
+      )
+    }
+    
+    # Overwite vector
     site <- df_site$site_pair
   }
   
@@ -73,6 +100,11 @@ import_sites_met_worker <- function(con, site, df_sites, start, end, summary,
     smonitor::import_processes() %>% 
     filter(site %in% !!site,
            variable %in% !!variables)
+  
+  # Return empty tibble if there are no processes
+  if (nrow(df_processes) == 0L) {
+    return(tibble())
+  }
   
   # Get observations, replace site if needed and join site name
   df <- import_with_sensor_id(
@@ -83,6 +115,7 @@ import_sites_met_worker <- function(con, site, df_sites, start, end, summary,
     end = end,
     site_name = FALSE,
     valid_only = valid_only,
+    set_invalid_values = set_invalid_values,
     tz = tz
   ) %>% 
     select(-sensing_element_id,
